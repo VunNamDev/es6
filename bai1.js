b1caa5c27817f5526051cb7967c3d3e6083fa984
VunNamDev <vunam.dev@gmail.com>
2018-05-11 11:55:01 +0700

    second edit

--- bai1.js
+++ bai1.js
@@ -3,9 +3,5 @@ let arr = fullName.split('...');
 const [,,lastName]  = [...arr];
 const [firstName,,] = [...arr];
 const [,name,] = [...arr];
-[...arr] = [lastName,name,firstName];
+arr = [lastName,name,firstName];
 console.log(...arr);
-
-const x =[2,8,5];
-[...x]= 'd';
-console.log(x);
